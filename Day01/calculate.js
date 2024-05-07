1fa9a4641865c1a4a24e93bea91c695a632945f0
ShashiSwain21 <168493526+ShashiSwain21@users.noreply.github.com>
2024-05-07 06:15:22 +0530

    Create calculate.js
    inside calculate.js import the `add` and `multiply` functions

---
+++ Day01/calculate.js
@@ -0,0 +1,11 @@
+// calculate.js
+import {add,multiply} from './mathOperations.js';
+import square from './mathOperations.js';
+
+const resultAdd =add(10,5);
+const resultMultiply =add(4,3);
+const resultSquare =square(9);
+
+console.log("Result of addition:",resultAdd);
+console.log("Result of multiplication:",resultMultiply);
+console.log("Square of 9 is:",resultSquare);
