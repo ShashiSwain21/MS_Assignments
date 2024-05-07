a16907d2a2aeaca150b9b07fc73bbbb478067902
ShashiSwain21 <168493526+ShashiSwain21@users.noreply.github.com>
2024-05-07 07:29:42 +0530

    Create NumberState.js
    Number State

---
+++ Day02/NumberState.js
@@ -0,0 +1,35 @@
+// NumberState.js
+import React, { useState } from 'react';
+
+const NumberState = () => {
+  // Initialize state variable 'number' with a starting value of 0
+  const [number, setNumber] = useState(0);
+  
+  // Function to increment the number
+  const handleIncrement = () => {
+    setNumber(prevNumber => prevNumber + 1);
+  };
+
+  // Function to decrement the number
+  const handleDecrement = () => {
+    setNumber(prevNumber => prevNumber - 1);
+  };
+
+  // Function to reset the number to 0
+  const handleReset = () => {
+    setNumber(0);
+  };
+
+  //to display the current state
+  return (
+    <div>
+      <h2>Number: {number}</h2>
+      {/* Buttons for interactions */}
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
+
+export default NumberState;
