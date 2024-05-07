60c50ee4aed3456b1270ede52aa1fe620b53a89b
ShashiSwain21 <168493526+ShashiSwain21@users.noreply.github.com>
2024-05-07 06:08:27 +0530

    Update mathOperations.js
    Inside `mathOperations.js`, define four functions: `add`, `subtract`, `multiply`, and `divide`, each
    accepting two parameters and returning their respective mathematical operation result.
    
    In `mathOperations.js`, create a function named `square` that takes a single parameter and
    returns its square.
    Export `square` as a default export.

--- Day01/mathOperations.js
+++ Day01/mathOperations.js
@@ -1,1 +1,19 @@
+//mathOperations.js
+export function add(num1,num2){
+  return num1+num2;
+}
 
+export function subtract(num1,num2){
+  return num1-num2;
+}
+
+export function multiply(num1,num2){
+  return num1 * num2;
+}
+export function divide(num1,num2){
+  return num1 / num2;
+}
+
+export default function square(num1){
+  return num1 * num1;
+}
